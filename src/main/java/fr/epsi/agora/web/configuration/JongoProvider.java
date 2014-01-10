b16a85d3ecc82d5bd908aad81033b44e9c7aa6ae
Khlio <thorgrim535@free.fr>
2014-01-10 16:31:16 +0100

    Oops!

--- src/main/java/fr/epsi/agora/web/configuration/JongoProvider.java
+++ src/main/java/fr/epsi/agora/web/configuration/JongoProvider.java
@@ -13,9 +13,9 @@ public class JongoProvider implements Provider<Jongo> {
 	@Override
 	public Jongo get() {
 		try {
-			DB db = new MongoClient().getDB("agora");
-			/*DB db = new MongoClient("dharma.mongohq.com", 10094).getDB("l63NezvVkeCikZVvYBmfw");
-			db.authenticate("dev-agora", "dev@gora".toCharArray());*/
+			//DB db = new MongoClient().getDB("agora");
+			DB db = new MongoClient("dharma.mongohq.com", 10094).getDB("l63NezvVkeCikZVvYBmfw");
+			db.authenticate("dev-agora", "dev@gora".toCharArray());
 			return new Jongo(db);
 		} catch (UnknownHostException e) {
 			throw new UnknownError("Impossible de démarrer jongo");
